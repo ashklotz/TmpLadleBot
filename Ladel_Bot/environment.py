f05f7bd04ac405d0a7ba9b91a1677d3b67f19c48
Ashlee Klotz <ashlee@ercspecialists.com>
2024-06-25 22:54:15 -0600

    fix env var typing

--- Ladel_Bot/environment.py
+++ Ladel_Bot/environment.py
@@ -1,6 +1,15 @@
 import os
 
 DISCORD_KEY = os.environ.get("DISCORD_KEY")
-GUILD_ID = os.environ.get("GUILD_ID")
-ASH_ID = os.environ.get("ASH_ID")
-TAY_ID = os.environ.get("TAY_ID")
+try:
+    GUILD_ID = int(os.environ.get("GUILD_ID"))
+except TypeError:
+    GUILD_ID = None
+try:
+    ASH_ID = int(os.environ.get("ASH_ID"))
+except TypeError:
+    ASH_ID = None
+try:
+    TAY_ID = int(os.environ.get("TAY_ID"))
+except TypeError:
+    TAY_ID = None
