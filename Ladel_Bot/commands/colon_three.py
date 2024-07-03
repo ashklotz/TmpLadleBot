c813fe29a3aacd857f615fc48d4b594cc11fac3d
Ashlee Klotz <ash.klotz64@gmail.com>
2024-07-02 21:18:11 -0600

    fix if statement

--- Ladel_Bot/commands/colon_three.py
+++ Ladel_Bot/commands/colon_three.py
@@ -2,6 +2,6 @@ import discord, random
 
 
 async def colon_three(message: discord.Message):
-    if random.randint(0, 15) == 0:
+    if random.randint(0, 15) == 0 and message.content == ":3":
         print(f"Sent :3 in response to {message.author.display_name}")
         await message.channel.send(":3")
