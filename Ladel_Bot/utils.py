32fb6b9af3f3ad3584e410be0809996827a9645a
Ashlee Klotz <ash.klotz64@gmail.com>
2024-08-06 21:17:19 -0600

    move utils

--- Ladel_Bot/utils.py
+++ Ladel_Bot/utils.py
@@ -20,3 +20,23 @@ def log_action(guild_id: int, messsage: str):
 
         log_channel_id = config.config_id
         # TODO the rest
+
+
+def update_role(guild_id, role_id, config_type):
+    with Session(environment.DB_ENGINE) as session:
+        statement = (
+            select(GuildConfig)
+            .where(GuildConfig.guild_id == guild_id)
+            .where(GuildConfig.config_type == config_type)
+        )
+        config = session.scalar(statement)
+        if not config:
+            config = GuildConfig(
+                guild_id=guild_id,
+                config_type=config_type,
+                config_id=role_id,
+            )
+        else:
+            config.config_id = role_id
+        session.add(config)
+        session.commit()
