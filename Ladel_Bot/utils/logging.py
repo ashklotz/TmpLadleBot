8b67e6440dfc5652ec837863c0e7bf1068c2cbad
Ashlee Klotz <ash.klotz64@gmail.com>
2024-08-06 20:52:34 -0600

    stub logging better

--- Ladel_Bot/utils/logging.py
+++ Ladel_Bot/utils/logging.py
@@ -1,5 +1,22 @@
 import discord
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from Ladel_Bot import environment, enums
+from Ladel_Bot.models import GuildConfig
 
 
-def log_action(guild: discord.Guild, messsage: str):
-    pass
+def log_action(guild_id: int, messsage: str):
+    guild: discord.Guild = None
+
+    with Session(environment.DB_ENGINE) as session:
+        statement = (
+            select(GuildConfig)
+            .where(GuildConfig.guild_id == guild_id)
+            .where(GuildConfig.config_type == enums.GuildConfig.log_channel)
+        )
+        config = session.scalar(statement)
+        if not config:
+            pass
+
+        log_channel_id = config.config_id
+        # TODO the rest
